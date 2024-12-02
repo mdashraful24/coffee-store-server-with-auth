dc58718c60ff8065cdac5167aa1f98a069b8ff24
mdashraful24 <mdashrafulislam2882@gmail.com>
2024-12-02 17:15:15 +0600

    added index.js file updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p8flg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -30,7 +30,7 @@ async function run() {
 
         const database = client.db('coffeeDB');
         const coffeeCollection = database.collection('coffee');
-
+        const userCollection = client.db('coffeeDB').collection('users');
 
         app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
@@ -61,7 +61,7 @@ async function run() {
                 $set: req.body
             }
 
-            const result = await coffeeCollection.updateOne(filter, updatedDoc, options )
+            const result = await coffeeCollection.updateOne(filter, updatedDoc, options)
 
             res.send(result);
         })
@@ -74,7 +74,38 @@ async function run() {
             res.send(result);
         })
 
+        // Users related apis
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            console.log('creating new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        app.patch('/users', async (req, res) => {
+            const email = req.body.email;
+            const filter = { email }
+            const updatedDoc = {
+                $set: {
+                    lastSignInTime: req.body?.lastSignInTime
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
 
     } finally {
         // Ensures that the client will close when you finish/error
